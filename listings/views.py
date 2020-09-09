21e6bf8a8b465f64d615863f3dbf8fff37529d3b
Miracle <collinsalex50@gmail.com>
2020-09-09 09:53:02 +0100

    integrated property search, fixed single listings, fixed search

--- listings/views.py
+++ listings/views.py
@@ -25,58 +25,59 @@ def listings(request):
 def single_listing(request, id, slug):
 
     # listing = get_object_or_404(Listing, id=id, slug=slug)
-    # context = {
-    #     'listing' : listing
-    # }
+    context = {
+        # 'listing' : listing
+    }
     return render(request, 'listings/single_listing.html', context)
 
 
 
 
 def search(request):
-    query_list = Listing.objects.order_by('-list_date') 
+    # query_list = Listing.objects.order_by('-list_date') 
 
-    # keywords
-    if 'keywords' in request.GET:   # checks for keyword search
-        keywords = request.GET['keywords']  
-        if keywords:     # makes sure keyword is not an empty string
-            # searches if the keyword is contained in the field description (case insensitive)
-            query_list = query_list.filter(description__icontains=keywords) 
+    # # keywords
+    # if 'keywords' in request.GET:   # checks for keyword search
+    #     keywords = request.GET['keywords']  
+    #     if keywords:     # makes sure keyword is not an empty string
+    #         # searches if the keyword is contained in the field description (case insensitive)
+    #         query_list = query_list.filter(description__icontains=keywords) 
 
-    # city
-    if 'city' in request.GET:
-        city = request.GET['city']
-        if city:
-            query_list = query_list.filter(city__iexact=city) # case insensitive exact search
+    # # city
+    # if 'city' in request.GET:
+    #     city = request.GET['city']
+    #     if city:
+    #         query_list = query_list.filter(city__iexact=city) # case insensitive exact search
 
-    # state
-    if 'state' in request.GET:
-        state = request.GET['state']
-        if state:
-            query_list = query_list.filter(state__iexact=state)
+    # # state
+    # if 'state' in request.GET:
+    #     state = request.GET['state']
+    #     if state:
+    #         query_list = query_list.filter(state__iexact=state)
 
-    # bedrooms
-    if 'bedrooms' in request.GET:
-        bedrooms = request.GET['bedrooms']
-        if bedrooms:
-            query_list = query_list.filter(bedrooms__lte=bedrooms)  # less than or equal to search 
+    # # bedrooms
+    # if 'bedrooms' in request.GET:
+    #     bedrooms = request.GET['bedrooms']
+    #     if bedrooms:
+    #         query_list = query_list.filter(bedrooms__lte=bedrooms)  # less than or equal to search 
 
-    # price
-    if 'price' in request.GET:
-        price = request.GET['price']
-        if price:
-            query_list = query_list.filter(price__lte=price)             
+    # # price
+    # if 'price' in request.GET:
+    #     price = request.GET['price']
+    #     if price:
+    #         query_list = query_list.filter(price__lte=price)             
 
     context = {
         # 'state_choices': state_choices,
         # 'bedroom_choices': bedroom_choices,
         # 'price_choices': price_choices,
-        'properties': query_list,
-        'values': request.GET
+        # 'properties': query_list,
+        # 'values': request.GET
     }
-    return render(request, 'listings/search.html', context) 
+    return render(request, 'listings/search_listings.html', context) 
 
 
 def test(request):
 
-    return render(request, 'listings/single_listings.html')
+    return render(request, 'listings/search_listings.html')
+    # return render(request, 'listings/single_listings.html')
