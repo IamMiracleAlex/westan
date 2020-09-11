9a140f3e78f7ac6a57fc050981fd8e3948062b01
Miracle <collinsalex50@gmail.com>
2020-09-11 22:50:39 +0100

    listings home, search, all listings complete

--- listings/views.py
+++ listings/views.py
@@ -32,9 +32,9 @@ def listings(request):
 
 def single_listing(request, id, slug):
 
-    # listing = get_object_or_404(Listing, id=id, slug=slug)
+    listing = get_object_or_404(Listing, id=id)
     context = {
-        # 'listing' : listing
+        'listing' : listing
     }
     return render(request, 'listings/single_listings.html', context)
 
@@ -42,82 +42,54 @@ def single_listing(request, id, slug):
 
 
 def search(request):
-
     results = []
-    
+
     query = request.GET['q'] if 'q' in request.GET else None
     type = request.GET['type'] if 'type' in request.GET else None
+    bedrooms = request.GET['bedrooms'] if 'bedrooms' in request.GET else None
+    bathrooms = request.GET['bathrooms'] if 'bathrooms' in request.GET else None
+    reference = request.GET['reference'] if 'reference' in request.GET else None
+    minprice = request.GET['minprice'] if 'minprice' in request.GET else None
+    maxprice = request.GET['maxprice'] if 'maxprice' in request.GET else None
 
-    queryset = Listing.objects.filter(type__iexact=type)
+
+    queryset = Listing.objects.filter(status=Listing.PUBLISHED)
+
+    if type:
+        queryset = queryset.filter(type__iexact=type)
 
     if query:
         queryset = queryset.filter(title__icontains=query)
 
+    if bedrooms:
+        queryset = queryset.filter(bedrooms__lte=bedrooms)
+
+    if bathrooms:
+        queryset = queryset.filter(bathrooms__lte=bathrooms)
+
+    if maxprice:
+        queryset = queryset.filter(maxprice__lte=maxprice)    
+
+    if minprice:
+        queryset = queryset.filter(minprice__gte=minprice)    
+
+    if reference:
+        queryset = Listing.objects.filter(reference=reference)
+
+    paginator = Paginator(queryset, 1) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        results = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an intger deliver the first page
+        results = paginator.page(1)
+    except EmptyPage:
+        #  if page is out of range deliver last page of results
+        results = paginator.page(paginator.num_pages)
+
+    
     context = {
-       
-        'listings': queryset,
+        'listings': results,
         'values': request.GET
     }
-    
     return render(request, 'listings/search_listings.html', context) 
-
-
-
-    # results = []
-    # query = None
-    
-    # if 'q' in request.GET:
-    #     query = request.GET['q']
-
-    #     queryset = Post.objects.filter(title__icontains=query)
-
-    #     paginator = Paginator(queryset, 1) # 3 posts per page
-    #     page = request.GET.get('page')
-    #     try:
-    #         results = paginator.page(page)
-    #     except PageNotAnInteger:
-    #         # if page is not an intger deliver the first page
-    #         results = paginator.page(1)
-    #     except EmptyPage:
-    #         #  if page is out of range deliver last page of results
-    #         results = paginator.page(paginator.num_pages)
-
-    
-    # return render(request, 'blog/blog_search.html', {'query': query,
-    #                                         'results': results})
-
-
-
-
-    # query_list = Listing.objects.order_by('-list_date') 
-
-    # # keywords
-    # if 'keywords' in request.GET:   # checks for keyword search
-    #     keywords = request.GET['keywords']  
-    #     if keywords:     # makes sure keyword is not an empty string
-    #         # searches if the keyword is contained in the field description (case insensitive)
-    #         query_list = query_list.filter(description__icontains=keywords) 
-
-    # # city
-    # if 'city' in request.GET:
-    #     city = request.GET['city']
-    #     if city:
-    #         query_list = query_list.filter(city__iexact=city) # case insensitive exact search
-
-    # # state
-    # if 'state' in request.GET:
-    #     state = request.GET['state']
-    #     if state:
-    #         query_list = query_list.filter(state__iexact=state)
-
-    # # bedrooms
-    # if 'bedrooms' in request.GET:
-    #     bedrooms = request.GET['bedrooms']
-    #     if bedrooms:
-    #         query_list = query_list.filter(bedrooms__lte=bedrooms)  # less than or equal to search 
-
-    # # price
-    # if 'price' in request.GET:
-    #     price = request.GET['price']
-    #     if price:
-    #         query_list = query_list.filter(price__lte=price)             
