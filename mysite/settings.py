bdb5ec354460a003e699a193db4086e54eab9bef
Miracle <collinsalex50@gmail.com>
2021-01-02 13:42:23 +0100

    removed email credentials from decouple

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,7 +138,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'westanhomesng@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = '@Westan-2020-Homes01'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Westan Homes <westanhomesng@gmail.com>'
